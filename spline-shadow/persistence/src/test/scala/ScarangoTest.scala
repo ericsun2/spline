3f5ffe4571ec8119c43eaa690986fe9a7e5e5cce
Vaclav Kosar <admin@vaclavkosar.com>
2018-12-19 18:23:41 +0100

    Sketch transformation

--- spline-shadow/persistence/src/test/scala/ScarangoTest.scala
+++ spline-shadow/persistence/src/test/scala/ScarangoTest.scala
@@ -30,11 +30,17 @@
  */
 
 
+import java.util.UUID
+
 import ArangoModel._
 import com.outr.arango._
 import com.outr.arango.managed._
+import org.apache.commons.lang.builder.ToStringBuilder.reflectionToString
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
+import za.co.absa.spline.{model => splinemodel}
+import za.co.absa.spline.model.{DataLineage, MetaDataset, TypedMetaDataSource}
+import za.co.absa.spline.model.op.{BatchWrite, Read, Write}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -42,9 +48,11 @@ import scala.concurrent.duration.Duration
 object ArangoModel {
   case class Progress(_id: Option[String], _key: Option[String], _rev: Option[String], timestamp: Long, readCount: Long) extends DocumentOption
   case class Execution(_id: Option[String], _key: Option[String], _rev: Option[String], appId: String, appName: String, sparkVer: String, timestamp: Long) extends DocumentOption
-  case class Schema(schema: Seq[String], _id: Option[String], _key: Option[String], _rev: Option[String]) extends DocumentOption
-  case class Operation(_id: Option[String], _key: Option[String], _rev: Option[String], name: String, expression: String) extends DocumentOption
-  case class DataSource(_id: Option[String], _key: Option[String], _rev: Option[String], name: String, path: String) extends DocumentOption
+//  case class Schema(attributes: Seq[String])
+  case class Operation(_id: Option[String], _key: Option[String], _rev: Option[String], name: String, expression: String, outputSchema: Seq[String]) extends DocumentOption
+  case class DataSource(`type`: String, path: String,  _rev: Option[String] = None, _id: Option[String] = None) extends DocumentOption {
+    val _key: Option[String] = Some(path)
+  }
   case class ProgressOf(_id: Option[String], _key: Option[String], _rev: Option[String], _from: String, _to: String) extends Edge with DocumentOption
 }
 
@@ -55,7 +63,6 @@ class ScarangoTest extends FunSpec with Matchers with MockitoSugar {
       object Database extends Graph("lineages") {
         val progress: VertexCollection[Progress] = vertex[Progress]("progress")
         val execution: VertexCollection[Execution] = vertex[Execution]("execution")
-        val schema: VertexCollection[Schema] = vertex[Schema]("schema")
         val operation: VertexCollection[Operation] = vertex[Operation]("operation")
         val dataSource: VertexCollection[DataSource] = vertex[DataSource]("dataSource")
         val progressOf: EdgeCollection[ProgressOf] = edge[ProgressOf]("progressOf", ("progress", "execution"))
@@ -70,6 +77,30 @@ class ScarangoTest extends FunSpec with Matchers with MockitoSugar {
       val query = aql"FOR f IN progress RETURN f"
 //      println(Await.result(Database.fruit.cursor(query), Duration.Inf))
     }
+  }
+
+  // TODO store full attribute information
+  private def findOutputSchema(dataLineage: DataLineage, operation: splinemodel.op.Operation) : Seq[String] = {
+    val metaDataset: MetaDataset = dataLineage.datasets.find((dts: MetaDataset) => dts.id == operation.mainProps.output).get
+    metaDataset.schema.attrs.map(attrId => {
+      dataLineage.attributes.find(_.id == attrId).get.name
+    })
+  }
+
+  def transform(dataLineage: DataLineage): (Execution, Set[DataSource], Set[Operation], Set[Operation]) = {
+    val execution = Execution(None, Some(dataLineage.id.toString), None, dataLineage.appId, dataLineage.appName, dataLineage.sparkVer, dataLineage.timestamp)
+    val operations = dataLineage.operations.map(op => {
+      val outputSchema = findOutputSchema(dataLineage, op)
+      Operation(None, Some(op.mainProps.id.toString), None, op.mainProps.name, reflectionToString(op), outputSchema)
+    })
+    val dataSources = dataLineage.operations.flatMap(op => op match {
+      case r: Read => r.sources.map(s => DataSource(r.sourceType, s.path))
+      case w: Write => Seq(DataSource(w.destinationType, w.path))
+    })
+    // progress for batch need to be generated during migration
+    val progress = dataLineage.operations.find(_.isInstanceOf[BatchWrite]).map(_ => Progress(None, None, None, dataLineage.timestamp, -1))
+    // TODO lineages are connected via meta dataset ids
+    null
   }
 }
 
