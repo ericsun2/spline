b78dc8be3a1142508fc9f452f04f9bd1e0ee02b5
Vaclav Kosar <admin@vaclavkosar.com>
2018-12-20 17:57:46 +0100

    Sketch transformation

--- spline-shadow/persistence/src/test/scala/ScarangoTest.scala
+++ spline-shadow/persistence/src/test/scala/ScarangoTest.scala
@@ -38,13 +38,14 @@ import com.outr.arango._
 import com.outr.arango.managed._
 import org.apache.commons.lang.builder.ToStringBuilder.reflectionToString
 import org.scalatest.mockito.MockitoSugar
+import org.scalatest.tools.Durations
 import org.scalatest.{FunSpec, Matchers}
 import za.co.absa.spline.{model => splinemodel}
 import za.co.absa.spline.model._
 import za.co.absa.spline.model.dt.Simple
 import za.co.absa.spline.model.op.{Operation => _, _}
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 
 object ArangoModel {
@@ -62,31 +63,30 @@ object ArangoModel {
 
 class ScarangoTest extends FunSpec with Matchers with MockitoSugar {
 
+  object Database extends Graph("lineages") {
+    val progress: VertexCollection[Progress] = vertex[Progress]("progress")
+    val execution: VertexCollection[Execution] = vertex[Execution]("execution")
+    val operation: VertexCollection[Operation] = vertex[Operation]("operation")
+    val dataSource: VertexCollection[DataSource] = vertex[DataSource]("dataSource")
+
+    val progressOf: EdgeCollection[ProgressOf] = edge[ProgressOf]("progressOf", ("progress", "execution"))
+    val follows: EdgeCollection[Follows] = edge[Follows]("follows", ("operation", "operation"))
+    val readsFrom: EdgeCollection[ReadsFrom] = edge[ReadsFrom]("readsFrom", ("operation", "dataSource"))
+    val writesTo: EdgeCollection[WritesTo] = edge[WritesTo]("writesTo", ("operation", "dataSource"))
+    val implements: EdgeCollection[Implements] = edge[Implements]("implements", ("execution", "operation"))
+  }
+
   describe("scarango") {
     it("funspec") {
-      object Database extends Graph("lineages") {
-        val progress: VertexCollection[Progress] = vertex[Progress]("progress")
-        val execution: VertexCollection[Execution] = vertex[Execution]("execution")
-        val operation: VertexCollection[Operation] = vertex[Operation]("operation")
-        val dataSource: VertexCollection[DataSource] = vertex[DataSource]("dataSource")
-
-        val progressOf: EdgeCollection[ProgressOf] = edge[ProgressOf]("progressOf", ("progress", "execution"))
-        val follows: EdgeCollection[Follows] = edge[Follows]("follows", ("operation", "operation"))
-        val readsFrom: EdgeCollection[ReadsFrom] = edge[ReadsFrom]("readsFrom", ("operation", "dataSource"))
-        val writesTo: EdgeCollection[WritesTo] = edge[WritesTo]("writesTo", ("operation", "dataSource"))
-        val implements: EdgeCollection[Implements] = edge[Implements]("implements", ("execution", "operation"))
-      }
       val result = Await.result(Database.init(), Duration.Inf)
-      println("Init result: " + result)
-      val execution: Execution = Await.result(Database.execution.insert(Execution("appId1", "appName1", "2.2", System.currentTimeMillis)), Duration.Inf)
-      val progress: Progress = Await.result(Database.progress.insert(Progress(System.currentTimeMillis, 10)), Duration.Inf)
-      val progressOf = Await.result(Database.progressOf.insert(ProgressOf(progress._id.get, execution._id.get)), Duration.Inf)
+//      println("Init result: " + result)
+//      val execution: Execution = Await.result(Database.execution.insert(Execution("appId1", "appName1", "2.2", System.currentTimeMillis)), Duration.Inf)
+//      val progress: Progress = Await.result(Database.progress.insert(Progress(System.currentTimeMillis, 10)), Duration.Inf)
+//      val progressOf = Await.result(Database.progressOf.insert(ProgressOf(progress._id.get, execution._id.get)), Duration.Inf)
       val query = aql"FOR f IN progress RETURN f"
+      save(datalineage())
     }
 
-    it("test tranformation") {
-      transform(datalineage())
-    }
   }
 
   // TODO store full attribute information
@@ -97,42 +97,88 @@ class ScarangoTest extends FunSpec with Matchers with MockitoSugar {
     })
   }
 
-  def transform(dataLineage: DataLineage): (Execution, Set[DataSource], Set[Operation], Set[Operation]) = {
+  private def awaitForever(future: Future[_]) = {
+    Await.result(future, Duration.Inf)
+  }
+
+  def save(dataLineage: DataLineage): Unit = {
     val execution = Execution(dataLineage.appId, dataLineage.appName, dataLineage.sparkVer, dataLineage.timestamp, Some(dataLineage.id.toString))
+    Database.execution.upsert(execution)
+
     val operations = dataLineage.operations.map(op => {
       val outputSchema = findOutputSchema(dataLineage, op)
       Operation(op.mainProps.name, reflectionToString(op), outputSchema, Some(op.mainProps.output.toString))
     })
-    dataLineage.operations.map(op => {
-//      op.mainProps.inputs.map(mdid => Follows(mdid)
+    operations.foreach(op => awaitForever(Database.operation.upsert(op)))
+
+    dataLineage.operations.foreach(op => {
+      op.mainProps.inputs
+        .map(mdid => Follows("operation/" + op.mainProps.output.toString, "operation/" + mdid.toString))
+        .foreach(f => awaitForever(Database.follows.insert(f)))
     })
-    val dataSources = dataLineage.operations.flatMap(op => op match {
-      case r: Read => r.sources.map(s => DataSource(r.sourceType, s.path))
-      case w: Write => Seq(DataSource(w.destinationType, w.path))
-    })
-    // progress for batch need to be generated during migration
-    val progress = dataLineage.operations.find(_.isInstanceOf[BatchWrite]).map(_ => Progress(dataLineage.timestamp, -1, Some(dataLineage.rootDataset.id.toString)))
-    val progressOf = progress.map(p => ProgressOf(execution._key.get, p._key.get, p._key))
-    // TODO implements
-    // TODO follows: operation tree
+//
+//    val dataSources = dataLineage.operations.flatMap(op => op match {
+//      case r: Read => r.sources.map(s => DataSource(r.sourceType, s.path))
+//      case w: Write => Seq(DataSource(w.destinationType, w.path))
+//    })
+//    dataSources.foreach(Database.dataSource.insert(_))
+//
+//    // progress for batch need to be generated during migration
+//    val progress = dataLineage.operations.find(_.isInstanceOf[BatchWrite]).map(_ => Progress(dataLineage.timestamp, -1, Some(dataLineage.rootDataset.id.toString)))
+//    progress.foreach(Database.progress.insert(_))
+//
+//    val progressOf = progress.map(p => ProgressOf(execution._key.get, p._key.get, p._key))
+//    progressOf.foreach(Database.progressOf.insert(_))
+//
+//    // TODO implements
+//    val implements = Implements(execution._key.get, dataLineage.rootOperation.mainProps.output.toString)
+//    Await.result(Database.implements.insert(implements), Duration.Inf)
+
     // TODO writesTo, readsFrom:
     // TODO Lineages are connected via meta dataset ids, should we store that somehow in progress events as well?
     null
   }
 
-  private def datalineage() = {
-    val dataType = Simple("int", nullable = true)
-    val attributes = Seq(
-      Attribute(randomUUID, "1", dataType.id),
-      Attribute(randomUUID, "2", dataType.id),
-      Attribute(randomUUID, "3", dataType.id)
-    )
-    val dataset = MetaDataset(randomUUID, Schema(attributes.map(_.id)))
-    val operation1 = BatchRead(OperationProps(randomUUID, "read", Seq.empty, dataset.id), "parquet", Seq(MetaDataSource("some/path_known", Nil)))
-    val operation2 = BatchRead(OperationProps(randomUUID, "read", Seq.empty, dataset.id), "parquet", Seq(MetaDataSource("some/path_unknown", Nil)))
+  private def datalineage(
+                           appId: String = "appId1",
+                           appName: String = "appName1",
+                           timestamp: Long = 123L,
+                           datasetId: UUID = randomUUID,
+                           path: String = "hdfs://foo/bar/path",
+                           append: Boolean = false)
+    : DataLineage = {
+      val dataTypes = Seq(Simple("StringType", nullable = true))
 
-    DataLineage("appId2", "appName2", 2L, "2.2", Seq(operation1, operation2), Seq(dataset), attributes, Seq(dataType))
-  }
+      val attributes = Seq(
+        Attribute(randomUUID(), "_1", dataTypes.head.id),
+        Attribute(randomUUID(), "_2", dataTypes.head.id),
+        Attribute(randomUUID(), "_3", dataTypes.head.id)
+      )
+      val aSchema = Schema(attributes.map(_.id))
+      val bSchema = Schema(attributes.map(_.id).tail)
+
+      val md1 = MetaDataset(datasetId, aSchema)
+      val md2 = MetaDataset(randomUUID, aSchema)
+      val md3 = MetaDataset(randomUUID, bSchema)
+      val md4 = MetaDataset(randomUUID, bSchema)
+
+      DataLineage(
+        appId,
+        appName,
+        timestamp,
+        "2.3.0",
+        Seq(
+          BatchWrite(OperationProps(randomUUID, "Write", Seq(md1.id), md1.id), "parquet", path, append),
+          Generic(OperationProps(randomUUID, "Union", Seq(md1.id, md2.id), md3.id), "rawString1"),
+          Generic(OperationProps(randomUUID, "Filter", Seq(md4.id), md2.id), "rawString2"),
+          Generic(OperationProps(randomUUID, "LogicalRDD", Seq.empty, md4.id), "rawString3"),
+          Generic(OperationProps(randomUUID, "Filter", Seq(md4.id), md1.id), "rawString4")
+        ),
+        Seq(md1, md2, md3, md4),
+        attributes,
+        dataTypes
+      )
+    }
 
 }
 
